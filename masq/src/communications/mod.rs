8d1da081f5d2bab5373495b717e511063cd282d6
Dan Wiebe <dnwiebe@gmail.com>
2020-08-30 01:24:54 -0400

    GH-301: Merge mistakes

--- masq/src/communications/mod.rs
+++ masq/src/communications/mod.rs
@@ -1,7 +1,5 @@
 // Copyright (c) 2019-2020, MASQ (https://masq.ai). All rights reserved.
 pub mod broadcast_handler;
-mod client_handle;
 mod client_listener_thread;
 pub mod connection_manager;
-mod node_connection;
 pub mod node_conversation;
