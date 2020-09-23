6617a04947926f0ff25addef0cc7673b75a8c4fc
Dan Wiebe <dnwiebe@gmail.com>
2020-09-23 06:50:42 -0400

    GH-301 Formatting

--- masq/src/notifications/crashed_notification.rs
+++ masq/src/notifications/crashed_notification.rs
@@ -10,7 +10,8 @@ pub struct CrashNotifier {}
 
 impl CrashNotifier {
     pub fn handle_broadcast(msg: MessageBody, stdout: &mut dyn Write, _stderr: &mut dyn Write) {
-        let (response, _) = UiNodeCrashedBroadcast::fmb(msg).expect(&format!("Bad UiNodeCrashedBroadcast:\n{:?}", msg));
+        let (response, _) = UiNodeCrashedBroadcast::fmb(msg)
+            .expect(&format!("Bad UiNodeCrashedBroadcast:\n{:?}", msg));
         if response.crash_reason == CrashReason::DaemonCrashed {
             exit_process(1, "The Daemon is no longer running; masq is terminating.");
         }
