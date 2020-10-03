0d5644d7703389ee6089f5eddd6d3e9889f1d327
Dan Wiebe <dnwiebe@gmail.com>
2020-10-02 23:51:57 -0400

    Added even more instrumentation and upgraded websocket 0.23.0 to 0.26.0: fingers crossed

--- masq/src/test_utils/mock_websockets_server.rs
+++ masq/src/test_utils/mock_websockets_server.rs
@@ -90,6 +90,8 @@ impl MockWebSocketsServer {
             let mut requests = inner_requests_arc.lock().unwrap();
             ready_tx.send(()).unwrap();
             log(do_log, index, "Waiting for upgrade");
+            // FIXME: This apparently can take wildly-inappropriate amounts of time to complete
+            // under Windows on Actions. (Sometimes more than 6hr.) Why?
             let upgrade = server.accept().unwrap();
             if upgrade
                 .protocols()
@@ -144,7 +146,11 @@ impl MockWebSocketsServer {
                     }
                 };
                 if let Some(incoming) = incoming_opt {
-                    log(do_log, index, "Recording incoming message");
+                    log(
+                        do_log,
+                        index,
+                        &format!("Recording incoming message: {:?}", incoming),
+                    );
                     requests.push(incoming.clone());
                     if let Ok(message_body) = incoming {
                         match message_body.path {
@@ -173,7 +179,14 @@ impl MockWebSocketsServer {
                                         client.send_message(&OwnedMessage::Text(outgoing)).unwrap()
                                     }
                                 }
-                                om => client.send_message(&om).unwrap(),
+                                om => {
+                                    log(
+                                        do_log,
+                                        index,
+                                        &format!("Responding with preset OwnedMessage: {:?}", om),
+                                    );
+                                    client.send_message(&om).unwrap()
+                                }
                             },
                             MessagePath::FireAndForget => {
                                 log(
